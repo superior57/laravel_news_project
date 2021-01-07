ea6cba698596034ba39b05690a6ac1dfda52f26c
talentedexpert0057 <petpanda0057@gmail.com>
2021-01-08 01:06:43 +0800

    changed sidebar's css a little

---
+++ database/seeds/db.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class db extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
